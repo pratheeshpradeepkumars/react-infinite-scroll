eea1e9513ffeacd1e321d9023a0d2f7c4c99f01c
pratheeshpradeepkumars <pratheeshps89@gmail.com>
2019-12-10 12:55:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-infinite-scroll-wp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-infinite-scroll-wp)
---
+++ index.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./style.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: 20,
+      loading: false
+    };
+  }
+  componentDidMount() {
+    // Detect when scrolled to bottom.
+    this.refs.myscroll.addEventListener("scroll", () => {
+      if (
+        this.refs.myscroll.scrollTop + this.refs.myscroll.clientHeight >=
+        this.refs.myscroll.scrollHeight
+      ) {
+        this.loadMore();
+      }
+    });
+  }
+
+  loadMore() {
+    this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState({ items: this.state.items + 20, loading: false });
+    }, 1000);
+  }
+
+  showItems() {
+    let items = [];
+    let i = 0;
+    while (i < this.state.items) {
+      items.push(<li key={i}>Item {i}</li>);
+      i++;
+    }
+    return items;
+  }
+
+  render() {
+    return (
+      <div ref="myscroll" style={{ height: "200px", overflow: "auto" }}>
+        <ul> {this.showItems()} </ul>
+        {this.state.loading ? <p className="App-intro">loading ...</p> : ""}
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
